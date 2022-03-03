a14475411e5334805b88d5f6485982cf25316f52
Tobi Oyeleye <krisella74@gmail.com>
2022-03-03 22:58:16 +0100

    setup auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "teacherpro5d833656": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import awsExports from "./aws-exports"
+import Amplify from "aws-amplify"
+
+Amplify.configure(awsExports)
 
 ReactDOM.render(
   <React.StrictMode>
